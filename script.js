b02837a414541b43b1e661a226eb9ba18a453686
jasonohdz1501 <jeissonolartehernandez@gmail.com>
2021-04-07 20:01:36 -0500

    upload js functions to footer
    new fuctions and modal windows for newsletter

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
+// Validate form newsletter
+$("#button-email").on('click', function(){
+    if ($("#emailOffer").val() == "") {
+      $("#message_error").text("Requerido");
+      $("#message_success").text("");
+      $("#emailOffer").addClass("error");
+    } else if (!isEmail($("#emailOffer").val())){
+      $("#message_error").text("Ingrese un email valido");
+      $("#message_success").text("");
+    } else {
+      $("#message_error").text("");
+      $("#message_success").text("Suscrito con éxito");
+      $(".popup-window").css("display","block");
+    }
+  });
 
+  $(".close").on('click',function(){
+    $(".popup-window").css("display","none");
+  });
 
+  function isEmail($email) {
+    var regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    return regex.test($email);
+  }
+ console.log(isEmail($("#emailOffer").val()));
